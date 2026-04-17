982494f408d769e35a16a41164978c84c54a5ac7
Rajat Kumar <grajatkumar54321@gmail.com>
2026-04-17 19:35:09 +0530

    updated api url

--- server/index.js
+++ server/index.js
@@ -21,12 +21,10 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
-)
+app.use(cors({
+  origin: ["https://your-vercel-app.vercel.app"],
+  credentials: true
+}));
 
 app.use(
 	fileUpload({
